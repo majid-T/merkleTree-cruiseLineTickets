18f8308b0c91bc05750038b66371c3ac854d74cd
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-07-27 03:15:59 +0430

    Added test Merkle root to deployment and test file | added merkle root test case

--- migrations/2_deployments.js
+++ migrations/2_deployments.js
@@ -1,6 +1,6 @@
 const CruiseMerkle = artifacts.require("CruiseMerkle");
 const voyageMerkleRoot =
-  "0x1234567890123456789012345678901234567890123456789012345678901234";
+  "0xcd18608fe9a0ddde7241a5c46407f3bafd9193e382c9baf11462d2acc4497ce7";
 
 module.exports = function (deployer) {
   deployer.then(async () => {
--- test/test-CruiseMerkle.js
+++ test/test-CruiseMerkle.js
@@ -2,7 +2,7 @@
 const CruiseMerkle = artifacts.require("CruiseMerkle.sol");
 // Test Root for now
 const voyageMerkleRoot =
-  "0x1234567890123456789012345678901234567890123456789012345678901234";
+  "0xcd18608fe9a0ddde7241a5c46407f3bafd9193e382c9baf11462d2acc4497ce7";
 
 // Contract test Begin
 contract("Cruise Merke Tree:", (accounts) => {
@@ -23,5 +23,19 @@ contract("Cruise Merke Tree:", (accounts) => {
       `Incorrent address length - length is ${contractInstance.address.length}`
     );
     assert.ok(contractInstance.address, "Contract not deployed..!!");
+  });
+
+  it("Contract is deployed with correct Merkle Root ", async () => {
+    const lineMerkleRootGetResult = await contractInstance.lineMerkleRoot();
+    assert.equal(
+      lineMerkleRootGetResult.length,
+      66,
+      "Received 'registeredUsersMerkleRoot' is not of bytes32 - 32*2+0x=66"
+    );
+    assert.equal(
+      lineMerkleRootGetResult,
+      voyageMerkleRoot,
+      "Deployed Merkle root is not what expected"
+    );
   });
 });
