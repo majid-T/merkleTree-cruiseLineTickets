af3e8eb2eb032d0983e8163ddb92b0a945174886
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-07-27 02:44:51 +0430

    Changes to smart contract

--- contracts/CruiseMerkle.sol
+++ contracts/CruiseMerkle.sol
@@ -10,17 +10,17 @@ contract CruiseMerkle {
         lineMerkleRoot = _lineMerkleRoot;
     }
 
-    /// @notice Calculates the leaf Hash of the user address
-    /// @param _leaf Address of the user
-    /// @return bytes32 leaf Hash of the user address
-    function hashForLeaf(address _leaf) private pure returns (bytes32) {
+    /// @notice Calculates the leaf Hash of the ticket
+    /// @param _leaf Hash of the ticket
+    /// @return bytes32 leaf Hash of the ticket
+    function hashForLeaf(bytes32 _leaf) private pure returns (bytes32) {
         return keccak256(abi.encodePacked(uint8(0x00), _leaf));
     }
 
-    /// @notice Calculates the node Hash of the user address
-    /// @param _left Address or Witness of the user (decided by '0x00 and 0x01')
-    /// @param _right Witness or address of the user (decided by '0x01 and 0x00')
-    /// @return bytes32 Node Hash of the user
+    /// @notice Calculates the node Hash of the ticket
+    /// @param _left Hash or witness of the user (decided by '0x00 and 0x01')
+    /// @param _right Witness or hash of the user (decided by '0x01 and 0x00')
+    /// @return bytes32 Node Hash of the ticket
     function hashForNode(bytes32 _left, bytes32 _right)
         private
         pure
@@ -29,20 +29,20 @@ contract CruiseMerkle {
         return keccak256(abi.encodePacked(uint8(0x01), _left, _right));
     }
 
-    /// @notice Generate a hash root base of input info
-    /// @param  _path Path (Index) for the issued ticket address
-    /// @param  _witnesseList Witnesses for issued ticket address
-    /// @param _inputAddress Address of the passanger who bought the ticket
-    /// @return  bytes32 Calculated root based on inpt data
-    function getRootForInput(
+    /// @notice Generate a hash root based on input info and compare with state lineROot
+    /// @param  _path Path (Index) for the ticket hash in the tree
+    /// @param  _witnesseList Witnesses for issued ticket
+    /// @param _ticketHash Hash of the ticket info
+    /// @return  bytes32 Calculated root based on input data
+    function isValidTicket(
         uint256 _path,
         bytes32[] memory _witnesseList,
-        address _inputAddress
-    ) private pure returns (bytes32) {
+        bytes32 _ticketHash
+    ) public view returns (bool) {
         uint256 path = _path;
         bytes32[] memory witnesses = _witnesseList;
-        address inputAddress = _inputAddress;
-        bytes32 ticketRoot = hashForLeaf(inputAddress); //  Calculates the leaf Hash of the passanger address
+        bytes32 ticketHash = _ticketHash;
+        bytes32 ticketRoot = hashForLeaf(ticketHash); //  Calculates the leaf Hash of the passanger ticket
         for (uint16 i = 0; i < witnesses.length; i++) {
             if ((path & 0x01) == 1) {
                 //  Calculations based on 0x01 - ticket root is on the right
@@ -53,6 +53,6 @@ contract CruiseMerkle {
             }
             path /= 2;
         }
-        return ticketRoot;
+        return ticketRoot == lineMerkleRoot;
     }
 }
