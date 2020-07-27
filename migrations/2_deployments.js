2b843706ebf6a4c831dfccdcfa9a2f652adbb2db
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-07-27 16:58:15 +0430

    Temp commit to remove project from GIThub

--- migrations/2_deployments.js
+++
@@ -1,9 +0,0 @@
-const CruiseMerkle = artifacts.require("CruiseMerkle");
-const voyageMerkleRoot =
-  "0xcd18608fe9a0ddde7241a5c46407f3bafd9193e382c9baf11462d2acc4497ce7";
-
-module.exports = function (deployer) {
-  deployer.then(async () => {
-    await deployer.deploy(CruiseMerkle, voyageMerkleRoot);
-  });
-};
