2f727cd80a1d31c9344f25f8a6addedfc9a6bcb0
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-12-16 17:50:01 -0500

    Modification to deploymeny.js file to deploy from account[0]

--- migrations/2_deployments.js
+++ migrations/2_deployments.js
@@ -1,9 +1,10 @@
 const CruiseMerkle = artifacts.require("CruiseMerkle");
 const voyageMerkleRoot =
   "0xcd18608fe9a0ddde7241a5c46407f3bafd9193e382c9baf11462d2acc4497ce7";
-
-module.exports = function (deployer) {
+  
+module.exports = function (deployer,network, accounts) {
+  console.log(`Deploying from ${accounts[0]}`)
   deployer.then(async () => {
-    await deployer.deploy(CruiseMerkle, voyageMerkleRoot);
+    await deployer.deploy(CruiseMerkle, voyageMerkleRoot,{from: accounts[0]});
   });
 };
