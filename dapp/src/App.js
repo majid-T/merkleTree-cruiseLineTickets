013802e3b9fb174813dafdc7a0ee773463562417
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-12-31 22:01:58 -0500

    Moved contract to redux store

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,66 +1,26 @@
 import "./App.css";
-import CruiseMerkle from "./contract/CruiseMerkle.json";
-import Web3 from "web3";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectNetworkId, setNetworkId } from "./features/contractSlice";
+import {
+  selectNetworkId,
+  selectLineMerkleRoot,
+  selectUserAccount,
+  selectContractAddress,
+  setBlockchainDataAsync,
+} from "./features/contractSlice";
 
 function App() {
   const networkId = useSelector(selectNetworkId);
+  const lineMerkleRoot = useSelector(selectLineMerkleRoot);
+  const account = useSelector(selectUserAccount);
+  const contractAddress = useSelector(selectContractAddress);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState("2");
-  const [contract, setContract] = useState();
-  const [account, setAccount] = useState();
   const [loading, setLoading] = useState(true);
-  const [merkleRoot, setMerkleRoot] = useState("");
-
-  const loadWeb3 = async () => {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum);
-      await window.ethereum.enable();
-    } else if (window.web3) {
-      window.web3 = new Web3(window.web3.currnetProvider);
-    } else {
-      window.alert(
-        "None Ethereum borswer - please use Meta mask or other wallet of your choice"
-      );
-    }
-  };
-
-  const loadContractAndAddress = async () => {
-    const netId = await window.web3.eth.net.getId();
-
-    const userAccount = await window.web3.eth.getAccounts();
-    setAccount(userAccount);
-
-    // Geting the contract
-    const contractNetworkData = await CruiseMerkle.networks[netId];
-
-    if (contractNetworkData) {
-      const merkleContract = await new window.web3.eth.Contract(
-        CruiseMerkle.abi,
-        contractNetworkData.address
-      );
-      setContract(merkleContract);
-    } else {
-      window.alert("Smart contract not deployed to detected network.");
-    }
-  };
 
   const loadBlockchainData = async () => {
-    //setting network to connect to wallet provide - here is BSC
-    await loadWeb3();
-
-    // load contract and address from the wallet
-    await loadContractAndAddress();
+    dispatch(setBlockchainDataAsync());
 
     setLoading(false);
-
-    //Test cotract by calling a view method
-    if (contract) {
-      const merkleRoot = await contract.methods.lineMerkleRoot().call();
-      setMerkleRoot(merkleRoot);
-    }
   };
 
   useEffect(() => {
@@ -72,9 +32,9 @@ function App() {
       {!loading && (
         <div className="container">
           <h1>BSC Test Net - network Id : {networkId}</h1>
-          <h4>Contract Address: {contract._address}</h4>
+          <h4>Contract Address: {contractAddress}</h4>
           <h4>Connected Account: {account}</h4>
-          <h4>Contract Merkle Root: {merkleRoot}</h4>
+          <h4>Contract Merkle Root: {lineMerkleRoot}</h4>
         </div>
       )}
     </div>
--- dapp/src/features/contractSlice.js
+++ dapp/src/features/contractSlice.js
@@ -1,20 +1,81 @@
 import { createSlice } from "@reduxjs/toolkit";
+import CruiseMerkle from "../contract/CruiseMerkle.json";
+import Web3 from "web3";
 
+let contract = null;
 export const contractSlice = createSlice({
   name: "contract",
   initialState: {
-    networkId: 10,
+    networkId: "",
+    userAccount: "",
+    lineMerkleRoot: "",
+    contractAddress: "",
   },
 
   reducers: {
-    setNetworkId: (state, id) => {
-      state.networkId = id;
+    setLineMerkleRoot: (state, action) => {
+      state.lineMerkleRoot = action.payload;
+    },
+    setNetworkId: (state, action) => {
+      state.networkId = action.payload;
+    },
+    setUserAccount: (state, action) => {
+      state.userAccount = action.payload;
+    },
+    setContractAddress: (state, action) => {
+      state.contractAddress = action.payload;
     },
   },
 });
 
-export const { setNetworkId } = contractSlice.actions;
+export const {
+  setLineMerkleRoot,
+  setNetworkId,
+  setUserAccount,
+  setContractAddress,
+} = contractSlice.actions;
 
+//Async function to set blokchain Data
+export const setBlockchainDataAsync = () => async (dispatch) => {
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    await window.ethereum.enable();
+  } else if (window.web3) {
+    window.web3 = new Web3(window.web3.currnetProvider);
+  } else {
+    window.alert(
+      "None Ethereum borswer - please use Meta mask or other wallet of your choice"
+    );
+  }
+
+  const netId = await window.web3.eth.net.getId();
+  dispatch(setNetworkId(netId));
+
+  const userAccount = await window.web3.eth.getAccounts();
+  dispatch(setUserAccount(userAccount));
+
+  //   Geting the contract
+  const contractNetworkData = await CruiseMerkle.networks[netId];
+
+  if (contractNetworkData) {
+    const merkleContract = await new window.web3.eth.Contract(
+      CruiseMerkle.abi,
+      contractNetworkData.address
+    );
+
+    dispatch(setContractAddress(merkleContract._address));
+    contract = merkleContract;
+
+    dispatch(setLineMerkleRoot(await contract.methods.lineMerkleRoot().call()));
+  } else {
+    window.alert("Smart contract not deployed to detected network.");
+  }
+};
+
+//selectors
 export const selectNetworkId = (state) => state.contract.networkId;
+export const selectUserAccount = (state) => state.contract.userAccount;
+export const selectLineMerkleRoot = (state) => state.contract.lineMerkleRoot;
+export const selectContractAddress = (state) => state.contract.contractAddress;
 
 export default contractSlice.reducer;
